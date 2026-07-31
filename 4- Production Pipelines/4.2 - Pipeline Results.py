90e057109aee0a343854ec68ed985a5a4ffe7e80
richardchristian8491@gmail.com <richardchristian8491@gmail.com>
2026-07-31 20:22:17 +0000

    initial

---
+++ 4- Production Pipelines/4.2 - Pipeline Results.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Explore Data
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM <catalog_name>.bookstore_etl.cn_daily_customer_books
